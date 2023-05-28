506f7503f4c195556b0652de53ccbf9f64f21b92
Seleez <bartekczernek10@gmail.com>
2023-05-28 16:46:05 +0200

    Chunk system generating planes

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -7,7 +7,6 @@ public class Chunk : MonoBehaviour
     public ComputeShader MarchingShader;
 
     public MeshFilter MeshFilter;
-
     public MeshCollider MeshCollider;
 
     ComputeBuffer _trianglesBuffer;
@@ -16,15 +15,10 @@ public class Chunk : MonoBehaviour
 
     public NoiseGenerator NoiseGenerator;
 
-    private void Awake() {
-        CreateBuffers();
-    }
+    [Range(0, 4)]
+    public int LOD;
 
-    private void OnDestroy() {
-        ReleaseBuffers();
-    }
-
-    struct Triangle {
+    public struct Triangle {
         public Vector3 a;
         public Vector3 b;
         public Vector3 c;
@@ -33,47 +27,89 @@ public class Chunk : MonoBehaviour
     }
 
     float[] _weights;
-    Mesh _mesh;
 
-    void Start() {
-        _weights = NoiseGenerator.GetNoise();
-        _mesh = new Mesh();
-
-        //MeshFilter.sharedMesh = ConstructMesh();
-        UpdateMesh();
+    private void Start() {
+        Create();
     }
 
-    Mesh ConstructMesh() {
-        MarchingShader.SetBuffer(0, "_Triangles", _trianglesBuffer);
-        MarchingShader.SetBuffer(0, "_Weights", _weightsBuffer);
+    private void OnValidate() {
+        if (Application.isPlaying) {
+            Create();
+        }
+    }
 
-        MarchingShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk);
+    void Create() {
+        CreateBuffers();
+        if (_weights == null) {
+            _weights = NoiseGenerator.GetNoise(GridMetrics.LastLod);
+        }
+
+        UpdateMesh();
+        ReleaseBuffers();
+    }
+
+    public void UpdateMesh() {
+        Mesh mesh = ConstructMesh();
+        MeshFilter.sharedMesh = mesh;
+        MeshCollider.sharedMesh = mesh;
+    }
+
+    public void EditWeights(Vector3 hitPosition, float brushSize, bool add) {
+        CreateBuffers();
+        int kernel = MarchingShader.FindKernel("UpdateWeights");
+
+        _weightsBuffer.SetData(_weights);
+        MarchingShader.SetBuffer(kernel, "_Weights", _weightsBuffer);
+
+        MarchingShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk(GridMetrics.LastLod));
+        MarchingShader.SetVector("_HitPosition", hitPosition);
+        MarchingShader.SetFloat("_BrushSize", brushSize);
+        MarchingShader.SetFloat("_TerraformStrength", add ? 1f : -1f);
+        MarchingShader.SetInt("_Scale", GridMetrics.Scale);
+
+        MarchingShader.Dispatch(kernel, GridMetrics.ThreadGroups(GridMetrics.LastLod), GridMetrics.ThreadGroups(GridMetrics.LastLod), GridMetrics.ThreadGroups(GridMetrics.LastLod));
+
+        _weightsBuffer.GetData(_weights);
+
+        UpdateMesh();
+        ReleaseBuffers();
+    }
+
+    public Mesh ConstructMesh() {
+        int kernel = MarchingShader.FindKernel("March");
+
+        MarchingShader.SetBuffer(kernel, "_Triangles", _trianglesBuffer);
+        MarchingShader.SetBuffer(kernel, "_Weights", _weightsBuffer);
+
+        float lodScaleFactor = GridMetrics.PointsPerChunk(GridMetrics.LastLod) / (float)GridMetrics.PointsPerChunk(LOD);
+
+        MarchingShader.SetFloat("_LodScaleFactor", lodScaleFactor);
+
+        MarchingShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk(GridMetrics.LastLod));
+        MarchingShader.SetInt("_LODSize", GridMetrics.PointsPerChunk(LOD));
         MarchingShader.SetFloat("_IsoLevel", .5f);
+        MarchingShader.SetInt("_Scale", GridMetrics.Scale);
 
         _weightsBuffer.SetData(_weights);
         _trianglesBuffer.SetCounterValue(0);
 
 
-        MarchingShader.Dispatch(0, GridMetrics.PointsPerChunk / GridMetrics.NumThreads, GridMetrics.PointsPerChunk / GridMetrics.NumThreads, GridMetrics.PointsPerChunk / GridMetrics.NumThreads);
+        MarchingShader.Dispatch(kernel, GridMetrics.ThreadGroups(LOD), GridMetrics.ThreadGroups(LOD), GridMetrics.ThreadGroups(LOD));
 
         Triangle[] triangles = new Triangle[ReadTriangleCount()];
         _trianglesBuffer.GetData(triangles);
 
         return CreateMeshFromTriangles(triangles);
     }
-    void UpdateMesh(){
-        Mesh mesh = ConstructMesh();
-        MeshFilter.sharedMesh = mesh;
-        MeshCollider.sharedMesh = mesh;
-        }
-    int ReadTriangleCount() {
+
+    public int ReadTriangleCount() {
         int[] triCount = { 0 };
         ComputeBuffer.CopyCount(_trianglesBuffer, _trianglesCountBuffer, 0);
         _trianglesCountBuffer.GetData(triCount);
         return triCount[0];
     }
 
-    Mesh CreateMeshFromTriangles(Triangle[] triangles) {
+    public Mesh CreateMeshFromTriangles(Triangle[] triangles) {
         Vector3[] verts = new Vector3[triangles.Length * 3];
         int[] tris = new int[triangles.Length * 3];
 
@@ -89,40 +125,17 @@ public class Chunk : MonoBehaviour
             tris[startIndex + 2] = startIndex + 2;
         }
 
-        //Mesh mesh = new Mesh();
-        //mesh.vertices = verts;
-        //mesh.triangles = tris;
-        //mesh.RecalculateNormals();
-
-        _mesh.Clear();
-        _mesh.vertices = verts;
-        _mesh.triangles = tris;
-        _mesh.RecalculateNormals();
-
-        return _mesh;
-    }
-    
-
-    private void OnDrawGizmos() {
-        if (_weights == null || _weights.Length == 0) {
-            return;
-        }
-        for (int x = 0; x < GridMetrics.PointsPerChunk; x++) {
-            for (int y = 0; y < GridMetrics.PointsPerChunk; y++) {
-                for (int z = 0; z < GridMetrics.PointsPerChunk; z++) {
-                    int index = x + GridMetrics.PointsPerChunk * (y + GridMetrics.PointsPerChunk * z);
-                    float noiseValue = _weights[index];
-                    Gizmos.color = Color.Lerp(Color.black, Color.white, noiseValue);
-                    Gizmos.DrawCube(new Vector3(x, y, z), Vector3.one * .3f);
-                }
-            }
-        }
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+        return mesh;
     }
 
     void CreateBuffers() {
-        _trianglesBuffer = new ComputeBuffer(5 * (GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk), Triangle.SizeOf, ComputeBufferType.Append);
+        _trianglesBuffer = new ComputeBuffer(5 * (GridMetrics.PointsPerChunk(LOD) * GridMetrics.PointsPerChunk(LOD) * GridMetrics.PointsPerChunk(LOD)), Triangle.SizeOf, ComputeBufferType.Append);
         _trianglesCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
-        _weightsBuffer = new ComputeBuffer(GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk, sizeof(float));
+        _weightsBuffer = new ComputeBuffer(GridMetrics.PointsPerChunk(GridMetrics.LastLod) * GridMetrics.PointsPerChunk(GridMetrics.LastLod) * GridMetrics.PointsPerChunk(GridMetrics.LastLod), sizeof(float));
     }
 
     void ReleaseBuffers() {
--- Assets/Scripts/NoiseGenerator.cs
+++ Assets/Scripts/NoiseGenerator.cs
@@ -14,40 +14,37 @@ public class NoiseGenerator : MonoBehaviour
     [SerializeField, Range(0f, 1f)] float groundPercent = 0.2f;
 
 
-    private void Awake() {
-        CreateBuffers();
-    }
 
-    private void OnDestroy() {
-        ReleaseBuffers();
-    }
-
-    public float[] GetNoise() {
+    public float[] GetNoise(int lod) {
+        CreateBuffers(lod);
         float[] noiseValues =
-            new float[GridMetrics.PointsPerChunk* GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk];
+           new float[GridMetrics.PointsPerChunk(lod) * GridMetrics.PointsPerChunk(lod) * GridMetrics.PointsPerChunk(lod)];
 
         NoiseShader.SetBuffer(0, "_Weights", _weightsBuffer);
 
-        NoiseShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk);
+        NoiseShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk(lod));
         NoiseShader.SetFloat("_NoiseScale", noiseScale);
         NoiseShader.SetFloat("_Amplitude", amplitude);
         NoiseShader.SetFloat("_Frequency", frequency);
         NoiseShader.SetInt("_Octaves", octaves);
         NoiseShader.SetFloat("_GroundPercent", groundPercent);
+        NoiseShader.SetInt("_Scale", GridMetrics.Scale);
+        NoiseShader.SetInt("_GroundLevel", GridMetrics.GroundLevel);
 
 
         NoiseShader.Dispatch(
-            0, GridMetrics.PointsPerChunk / GridMetrics.NumThreads, GridMetrics.PointsPerChunk / GridMetrics.NumThreads, GridMetrics.PointsPerChunk / GridMetrics.NumThreads
-        );
+                 0, GridMetrics.ThreadGroups(lod), GridMetrics.ThreadGroups(lod), GridMetrics.ThreadGroups(lod)
+             );
 
         _weightsBuffer.GetData(noiseValues);
 
+        ReleaseBuffers();
         return noiseValues;
     }
 
-    void CreateBuffers() {
+    void CreateBuffers(int lod) {
         _weightsBuffer = new ComputeBuffer(
-            GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk, sizeof(float)
+            GridMetrics.PointsPerChunk(lod) * GridMetrics.PointsPerChunk(lod) * GridMetrics.PointsPerChunk(lod), sizeof(float)
         );
     }
 
